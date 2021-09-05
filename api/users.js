2ba944f823e340603ae57dab6048f5d3703c8131
jonathanng1122 <jonathanng1122@gmail.com>
2021-09-04 17:00:06 -0700

    edited comment in api/users

--- api/users.js
+++ api/users.js
@@ -1,6 +1,6 @@
 import db from '@react-native-firebase/firestore';
 const User = db().collection('users');
-//needs to be either a cb, a promise or a async/await
+//needs to be a async/await
 export const getUser = async (uid) => {
     return await User.doc(uid).get();
 }
