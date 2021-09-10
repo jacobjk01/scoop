97b1651aa0545a77f500318ea9a5792774c54604
Josh Wong <joshwong74@gmail.com>
2021-09-09 23:15:45 -0700

    updated api/users.js to database design functions, added get to api/utilities.js

--- api/users.js
+++ api/users.js
@@ -1,5 +1,5 @@
 import db from '@react-native-firebase/firestore';
-import { update } from './utilities';
+import { update, get } from './utilities';
 
 const User = db().collection('users');
 //needs to be a async/await
@@ -9,13 +9,13 @@ const User = db().collection('users');
 //in this case, getUser will create a basic document for the user
 //however that is below the abstraction
 export const getUser = async (userAuth) => {
-    let user = await User.doc(userAuth.uid).get();
+    let user = await get(User, userAuth.uid);
     if (!user._exists) {            
         await User.doc(userAuth.uid).set({
             userType: 'user',
             username: userAuth.displayName
         })
-        return await User.doc(userAuth.uid).get();
+        return await get(User, userAuth.uid);
     }
     // TODO update profile pic
     return user
@@ -37,81 +37,27 @@ export const changeMajor = (uuid, major) => {
     await update(User, uid, "major", major)
 }
 
-export const changeYear = (uuid, year) => {
-    db()
-        .collection('users')
-        .doc(uuid)
-        .update({
-            year: year
-        })
-        .then(() => {
-            console.log('Year updated!')
-        })
-        .catch(err => {
-            console.error(err)
-        })
+export const changeYear = (uid, year) => {
+    await update(User, uid, "year", year)
 }
 
-export const changeIntro = (uuid, intro) => {
-    db()
-        .collection('users')
-        .doc(uuid)
-        .update({
-            intro: intro,
-        })
-        .then(() => {
-            console.log('Intro updated!')
-        })
-        .catch(err => {
-            console.error(err)
-        })
+export const changeIntro = (uid, intro) => {
+    await update(User, uid, "intro", intro)
 }
 
 // Represented as a list or from multi-selection
-export const changeLanguages = (uuid, languages) => {
-    db()
-        .collection('users')
-        .doc(uuid)
-        .update({
-            languages: languages,
-        })
-        .then(() => {
-            console.log('Languages updated!')
-        })
-        .catch(err => {
-            console.error(err)
-        })
+export const changeLanguages = (uid, languages) => {
+    await update(User, uid, "languages", languages)
 }
 
-export const changeHometown = (uuid, hometown) => {
-    db()
-        .collection('users')
-        .doc(uuid)
-        .update({
-            hometown: hometown,
-        })
-        .then(() => {
-            console.log('Hometown updated!')
-        })
-        .catch(err => {
-            console.error(err)
-        })
+export const changeHometown = (uid, hometown) => {
+    await update(User, uid, "hometown", hometown)
 }
+
 
 // Will need to send the full list of tours back, and update all
-export const changeTours = (uuid, tours) => {
-    db()
-        .collection('users')
-        .doc(uuid)
-        .update({
-            tours: tours,
-        })
-        .then(() => {
-            console.log('Tours updated!')
-        })
-        .catch(err => {
-            console.error(err)
-        })
+export const changeTours = (uid, tours) => {
+    await update(User, uid, "tours", tours)
 }
 
 // Specifics for searching may be different
@@ -130,44 +76,3 @@ export const searchTourGuides = (searchQuery) => {
             console.error(err)
         })
 }
-
-
-/* TESTING
-
-// changeUsername(userId, "username")
-
-export function addUser(userId, username) {
-    db.firestore()
-    .collection('users')
-    .add({
-        name: user.name,
-        createdAt: firebase.firestore.FieldValue.serverTimestamp()
-    }).then((data) => addComplete(data))
-    .catch((error) => console.log(error));
-}
-
-export async function getUsername(userRetrieved) {
-    var snapshot = await firebase.firestore()
-    .collection('users')
-    .orderBy('createdAt')
-    .get()
-    snapshot.forEach((doc) => {
-
-    })
-}
-
-
-
-// changePassword()
-onChangePasswordPress = () => {
-    var user = firebase.auth().currentUser;
-    user.updatePassword(this.state.newPassword).then(() => {
-        Alert.alert("Password was changed");
-    }).catch((error) => {
-        Alert.alert(error.message);
-    });
-}
-
-render()
-
-*/
--- api/utilities.js
+++ api/utilities.js
@@ -10,3 +10,7 @@ export async function update(collection, id, key, value) {
     }
     return true
 }
+
+export async function get(collection, id) {
+    return await collection.doc(id).get();
+}
