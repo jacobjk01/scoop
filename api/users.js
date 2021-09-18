fdb40d2989199649b4849b23e61c8c977d2fb2e3
jonathanng1122 <jonathanng1122@gmail.com>
2021-09-18 14:19:03 -0700

    fixed usercontroller conflict

--- api/users.js
+++ api/users.js
@@ -1,27 +1,4 @@
 import db from '@react-native-firebase/firestore';
-<<<<<<< HEAD
-const User = db().collection('users');
-import {get} from './auth'
-//needs to be a async/await
-
-
-//gets the user, if there is no document for the user, first time
-//signers may not have a user in the users collection
-//in this case, getUser will create a basic document for the user
-//however that is below the abstraction
-export const getUser = async (userAuth) => {
-    let user = await get(User, userAuth.uid);
-    if (!user._exists) {            
-        await User.doc(userAuth.uid).set({
-            userType: 'user',
-            username: userAuth.displayName
-        })
-        return await get(User, userAuth.uid);
-    }
-    // TODO update profile pic
-    return user
-}
-=======
 import { update, get } from './utilities';
 
 const Users = db().collection('users');
@@ -113,4 +90,3 @@ export const searchTourGuides = (searchQuery) => {
             console.error(err)
         })
 }
->>>>>>> origin/usercontroller
