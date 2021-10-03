fa6f2135a78641def30db6428ad64766ec50951e
jonathanng1122 <jonathanng1122@gmail.com>
2021-10-03 10:36:16 -0700

    refactored homepage

---
+++ data/toursData.js
@@ -0,0 +1,26 @@
+module.exports = {
+    images: [
+        {name: 'Santa Monica', src: require('../images/SantaMonica.png')},
+        {name: 'Westwood Tour', src: require('../images/Westwood_village.png')},
+    ],
+    guides: [
+        {
+          name: 'Natalie',
+          year: 'Junior',
+          major: 'Psychobiology',
+          src: require('../images/natalie.png'),
+        },
+        {
+          name: 'Trevor',
+          year: 'Senior',
+          major: 'Marketing',
+          src: require('../images/trevor.png'),
+        },
+        {
+          name: 'Brittany',
+          year: 'Junior',
+          major: 'Mechanical Eng.',
+          src: require('../images/brittany.png'),
+        },
+      ]
+}
