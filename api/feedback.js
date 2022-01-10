bf3645fb496be56b6d93bbd3d69cc11054452615
awang923 <awang144@ucr.edu>
2022-01-09 21:17:44 -0800

    added feedback api

---
+++ api/feedback.js
@@ -0,0 +1,40 @@
+import db from '@react-native-firebase/firestore';
+
+import { update, get} from './utilities';
+import firebase from 'firebase/app' 
+import { useLinkBuilder } from '@react-navigation/native';
+import {collection, query, orderBy, serverTimestamp} from 'firebase/firestore';
+
+const Feedback = db().collection("feedback");
+
+export const sendFeedback = async(id, topic, message)=>{
+    if(!(topic && message)){
+        console.log("missing parameter")
+        return;
+    }
+    else{
+        
+        await Feedback.doc(id).set({
+            user: id,
+            topic,
+            message, 
+            timestamp: new Date(),
+        });
+        await Feedback.doc(id).collection("admin").doc().set({
+            assigned: "user",
+            status: "in progress",
+            isArchived: false,
+        });
+        
+    }
+    return Feedback;
+}
+
+//not tested yet
+export const editFeedback = async(id) => {
+    await Feedback.doc(id).collection("admin").doc().update({
+        status: "complete",
+        isArchived: true,
+    });
+    return Feedback;
+}
