6ed569a5705e1938b1c7eb48a1cb5aef06b32acf
jonathanng1122 <jonathanng1122@gmail.com>
2022-05-28 19:45:58 -0500

    f

--- utils/index.js
+++ utils/index.js
@@ -1,6 +1,7 @@
 
 
 export function capitalizeFirstLetter(string) {
+  if (!string) throw new Error('calling capitalizeFirstLetter: ' + string + ' is not a string')
   string = string.toLowerCase();
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
